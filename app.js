2c2090b730db2df8e4182d85b26fa328857bfd05
dasEinhorn1 <achayward1@gmail.com>
2020-04-13 13:45:24 -0400

    adds port option so we don't step on other servers toes

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import helmet from 'helmet';
+import config from './.config';
 
 import routes from './routes';
 
 const app = express();
+const PORT = config.port || 80;
 
 app.use(helmet());
 app.use(bodyParser.json());
@@ -14,7 +16,7 @@ app.get('/', (req, res) => res.send('hello world'));
 
 app.use('/v1', routes);
 
-app.listen(80, () => console.log('Pivot app deployed on port 80'));
+app.listen(PORT, () => console.log(`Pivot app deployed on port ${PORT}`));
 
 module.exports = {
   app,
